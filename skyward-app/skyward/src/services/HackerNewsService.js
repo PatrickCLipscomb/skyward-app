305b22524008c848339a3b641f766a76d487d460
PatrickCLipscomb <patrick74lipscomb@gmail.com>
2019-04-30 10:44:11 -0700

    remove commented code

--- skyward-app/skyward/src/services/HackerNewsService.js
+++ skyward-app/skyward/src/services/HackerNewsService.js
@@ -21,10 +21,7 @@ const Api = {
     return makeFetch(url);
   },
   async fetchNewStories(storyIds, pagination) {
-    // const lower = pagination === 1 ? 0 : pagination / 5 * 100;
-    // const upper = pagination === 1 ? 100 : (pagination / 5 + 1) * 100;
     const storyIdList = storyIds.slice(0, 240)
-    // console.log('from fetchNewStories', lower, upper, pagination, storyIdList) 
     const actions = storyIdList.map(this.fetchSingleStory);
     const articles = await Promise.all(actions)
     return articles
